90972a134037eafbda562f562776c323f889176a
JianXiongZ <jianxiong@canaan-creative.com>
2017-08-25 15:55:44 +0800

    shell command output information is redirected to python output

--- mining.py
+++ mining.py
@@ -1,36 +1,53 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #encoding: utf-8
 
 import cgi, cgitb
-import os
-import json
+#import json
+import sys
+import subprocess
+import time 
 
 form = cgi.FieldStorage()
-
 value = form.getvalue('config')
 
-PATH = '~/eth_miner/config.txt'
-
-def mining(args):
-	for i in args:
-		if len(i) == 0:
-			print(json.dumps("non_input"))
-			return None
-	fhandler = open(PATH, 'wt')
-	if len(args) == 3:
-		fhandler.write('-epool ' + args[0] + '\n-ewal ' + args[1] + '\n-epsw ' + args[2] + '\n-mode 1')
-	elif len(args) == 4:
-		fhandler.write('-epool ' + args[0] + '\n-ewal ' + args[1] + '\n-epsw ' + args[2] + '\n-eworker ' + args[3] +'\n-mode 1')
-	elif len(args) == 6:
-		fhandler.write('-epool ' + args[0] + '\n-ewal ' + args[1] + '\n-epsw ' + args[2] + '\n-dpool ' + args[3] + '\n-dwal ' + args[4] + '\n-dpasw ' + args[5])
-	elif len(args) == 7:
-		fhandler.write('-epool ' + args[0] + '\n-ewal ' + args[1] + '\n-epsw ' + args[2] + '\n-eworker ' + args[3] + '\n-dpool ' + args[4] + '\n-dwal ' + args[5] + '\n-dpasw ' + args[6])
-
-	fhandler.close()
-	#print(os.system('/home/canaan/eth_miner/ethdcrminer64'))
-	
+args = value.split(',')
+PATH = '/home/jianxiong/eth_miner/config.txt'
 
 
-	
+print("Content-type: text/html\n")
 
-mining(value.split(','))#value is list or tuple
+fhandler = open(PATH, 'wt')
+
+for i in args:
+    if len(i) == 0:
+        print("non_input")
+        sys.exit()
+
+if len(args) == 3:
+    fhandler.write('-epool ' + args[0] + '\n-ewal ' + args[1] + '\n-epsw ' + args[2] + '\n-mode 1')
+elif len(args) == 4:
+    fhandler.write('-epool ' + args[0] + '\n-ewal ' + args[1] + '\n-epsw ' + args[2] + '\n-eworker ' + args[3] +'\n-mode 1')
+elif len(args) == 6:
+    fhandler.write('-epool ' + args[0] + '\n-ewal ' + args[1] + '\n-epsw ' + args[2] + '\n-dpool ' + args[3] + '\n-dwal ' + args[4] + '\n-dpasw ' + args[5])
+elif len(args) == 7:
+    fhandler.write('-epool ' + args[0] + '\n-ewal ' + args[1] + '\n-epsw ' + args[2] + '\n-eworker ' + args[3] + '\n-dpool ' + args[4] + '\n-dwal ' + args[5] + '\n-dpasw ' + args[6])
+
+fhandler.close()
+
+
+result = subprocess.Popen('/var/www/html/eth_test/test.sh', stdout=subprocess.PIPE)
+
+sys.stdout.write("Transfer-Encoding: chunked\r\n")
+sys.stdout.write("Content-Type: text/html\r\n")
+
+def chunk(msg=""): #why????
+	return "\r\n%X\r\n%s" % ( len( msg ) , msg )
+
+while True:
+	time.sleep(.1)
+	line = result.stdout.readline()
+	if line:
+		sys.stdout.write(chunk(line.decode('utf-8')))
+		sys.stdout.flush()
+	else:
+		break
